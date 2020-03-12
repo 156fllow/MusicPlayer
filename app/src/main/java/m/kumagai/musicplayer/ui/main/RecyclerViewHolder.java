924301db9a6fd533a4c39501c405cdf5639beecd
fire156flow <fire156flow@gmail.com>
2020-03-12 22:49:44 +0900

    最終コミット

---
+++ app/src/main/java/m/kumagai/musicplayer/ui/main/RecyclerViewHolder.java
@@ -0,0 +1,25 @@
+package m.kumagai.musicplayer.ui.main;
+
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+import m.kumagai.musicplayer.R;
+
+public class RecyclerViewHolder extends RecyclerView.ViewHolder {
+
+    public TextView titleview;
+    public TextView artistview;
+    public TextView albumview;
+    public LinearLayout linearLayout;
+
+    public RecyclerViewHolder(View itemView){
+        super(itemView);
+        titleview = itemView.findViewById(R.id.title);
+        artistview = itemView.findViewById(R.id.artist);
+        albumview = itemView.findViewById(R.id.album);
+        linearLayout = itemView.findViewById(R.id.row);
+    }
+
+}
